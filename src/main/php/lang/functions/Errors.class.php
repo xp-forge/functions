77e2567cacdec9f009f5d96aaccf6d3ee6f77122
Timm Friebe <thekid@thekid.de>
2015-11-16 00:29:51 +0100

    QA: Add apidocs

--- src/main/php/lang/functions/Errors.class.php
+++ src/main/php/lang/functions/Errors.class.php
@@ -3,29 +3,57 @@
 use lang\Throwable;
 use lang\Error;
 
+/**
+ * Factory for error handlers to be used in conjunction with `Closure::wrapIn()`.
+ *
+ * @test  xp://lang.functions.unittest.ErrorsTest
+ */
 abstract class Errors {
 
+  /**
+   * Suppress errors and return null
+   *
+   * @return function(php.Closure, var): var
+   */
   public static function suppress() {
-    return self::error(function($e) { return null; });
+    return self::handleWith(function($e) { return null; });
   }
 
+  /**
+   * Handle errors and return what the given handler returns
+   *
+   * @param  function(lang.Throwable): var $with The handler
+   * @return function(php.Closure, var): var
+   */
   public static function handle($with) {
-    return self::error(Functions::$APPLY->cast($with));
+    return self::handleWith(Functions::$APPLY->cast($with));
   }
 
+  /**
+   * Handle errors and throw what the given handler returns
+   *
+   * @param  function(lang.Throwable): lang.Throwable $as The handler
+   * @return function(php.Closure, var): var
+   */
   public static function rethrow($as) {
     $func= Functions::$APPLY->cast($as);
-    return self::error(function($e) use($func) { throw $func($e); });
+    return self::handleWith(function($e) use($func) { throw $func($e); });
   }
 
-  private static function error($handle) {
-    return function($block, $val) use($handle) {
+  /**
+   * Helper method for the above public methods
+   *
+   * @param  function(lang.Throwable): var $handler
+   * @return function(php.Closure, var): var
+   */
+  private static function handleWith($handler) {
+    return function($block, $val) use($handler) {
       try {
         return $block($val);
       } catch (Throwable $e) {
-        return $handle($e);
+        return $handler($e);
       } catch (\Throwable $e) {
-        return $handle(new Error(get_class($e).': '.$e->getMessage()));
+        return $handler(new Error(get_class($e).': '.$e->getMessage()));
       }
     };
   }
