7325305594770ce21db08dd9c63efbedab22de14
Timm Friebe <thekid@thekid.de>
2015-11-16 21:28:23 +0100

    Remove forList()

--- src/main/php/lang/functions/Closures.class.php
+++ src/main/php/lang/functions/Closures.class.php
@@ -39,39 +39,4 @@ abstract class Closures {
     }
     throw new IllegalArgumentException('Expected either an array or an object overloading array access');
   }
-
-  /**
-   * Returns a closure for list lookup
-   *
-   * @param  php.ArrayAccess|var[] $list
-   * @return lang.functions.Closure
-   * @throws lang.IllegalArgumentException
-   */
-  public static function forList($list) {
-    if (func_num_args() > 1) {
-      $default= func_get_arg(1);
-      if ($list instanceof \ArrayAccess) {
-        return Closure::of(function($offset) use($list, $default) {
-          return isset($list[$offset]) ? $list[$offset] : $default;
-        });
-      } else if (is('var[]', $list)) {
-        return Closure::of(function($offset) use($list, $default) {
-          return array_key_exists($offset, $list) ? $list[$offset] : $default;
-        });
-      }
-    } else {
-      if ($list instanceof \ArrayAccess) {
-        return Closure::of(function($offset) use($list) {
-          if (isset($list[$offset])) return $list[$offset];
-          throw new ElementNotFoundException('No element at offset #'.$offset);
-        });
-      } else if (is('var[]', $list)) {
-        return Closure::of(function($offset) use($list) {
-          if (array_key_exists($offset, $list)) return $list[$offset];
-          throw new ElementNotFoundException('No element at offset #'.$offset);
-        });
-      }
-    }
-    throw new IllegalArgumentException('Expected either an array or an object overloading array access');
-  }
 }
